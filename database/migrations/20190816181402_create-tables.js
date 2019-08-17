ba5a19a731c5b248da85e873f11177e0c3e61aba
Cedricium <amaya.cedric@gmail.com>
2019-08-16 18:49:35 -0700

    Finsh coding database relationships and create database

--- database/migrations/20190816181402_create-tables.js
+++ database/migrations/20190816181402_create-tables.js
@@ -1,39 +1,56 @@
 
 exports.up = function(knex) {
-  knex.schema.createTable('projects', table => {
-    table.increments('id')
-    table
-      .string('name')
-      .notNullable()
-    table.string('description')
-    table
-      .boolean('completed')
-      .notNullable()
-      .defaultTo(false)
-  })
+  return knex.schema
+    .createTable('projects', table => {
+      table.increments()
+      table.string('name')
+        .notNullable()
+      table.string('description')
+      table.boolean('completed')
+        .notNullable()
+        .defaultTo(false)
+    })
 
-  knex.schema.createTable('resources', table => {
-    table.increments('id')
-    table
-      .string('name')
-      .unique()
-      .notNullable()
-    table.string('description')
-  })
+    .createTable('resources', table => {
+      table.increments()
+      table.string('name')
+        .unique()
+        .notNullable()
+      table.string('description')
+    })
 
-  knex.schema.createTable('tasks', table => {
-    table.increments('id')
-    table
-      .string('description')
-      .notNullable()
-    table.string('notes')
-    table
-      .boolean('completed')
-      .notNullable()
-      .defaultTo(false)
-  })
+    .createTable('tasks', table => {
+      table.increments()
+      table.string('description')
+        .notNullable()
+      table.string('notes')
+      table.boolean('completed')
+        .notNullable()
+        .defaultTo(false)
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+    })
+
+    .createTable('project_resources', table => {
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('projects.id')
+      table.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resources.id')
+      table.primary(['project_id', 'resource_id'])
+    })
 }
 
 exports.down = function(knex) {
-
+  return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
 }
