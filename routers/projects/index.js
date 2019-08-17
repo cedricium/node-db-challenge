4e421588650fed579b6ecad42236bcc010b80bb4
Cedricium <amaya.cedric@gmail.com>
2019-08-16 21:50:05 -0700

    Correct binary representation of boolean for projects

--- routers/projects/index.js
+++ routers/projects/index.js
@@ -3,7 +3,12 @@ const db = require('../../database/config')
 
 router.get('/', async (req, res) => {
   try {
-    const projects = await db('projects')
+    const projects = (await db('projects')).map(project => {
+      return {
+        ...project,
+        completed: project.completed === 0 ? false : true
+      }
+    })
     res.json(projects)
   } catch (error) {
     res.status(500).json({
